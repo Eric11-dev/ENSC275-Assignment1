fbf0d8345576e6b2c2122ffbebea3db38ffbd96f
erichuang11 <eric.h@gokabu.ca>
2020-10-02 02:31:18 -0700

    fifth submit

--- src/com/eric/ensc275/Main.java
+++ src/com/eric/ensc275/Main.java
@@ -1,7 +1,16 @@
 package com.eric.ensc275;
 
 public class Main {
-
+    /**
+     * Simple implementation of RLC circuit q(t) oscillates.
+     * This application generate log data and simple plot of q(t) in given running time
+     * The Plot class is the source from github author Yuriy Guskov
+     *
+     * @author: ZhaoQi Huang (Eric)
+     * @Student#: 301320726
+     * @Sfu mail: zha44@sfu.ca
+     *
+     */
     public static void main(String[] args) {
 	// Execute The Code
         new GUI();
